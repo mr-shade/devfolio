2855c64162bf3d39a67f14bf3ccd43fd8711e1d4
SH20RAJ <shaswatraj3@gmail.com>
2025-05-20 09:33:25 +0530

    Update ESLint configuration to disable specific React rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/jsx-no-target-blank": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  }
+
 ];
 
 export default eslintConfig;
